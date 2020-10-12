6e639ce9f6a4847c91b40bd59c2c288bede6f861
Crowdbotics <team@crowdbotics.com>
2020-10-12 13:52:57 +0000

    Initial commit.

---
+++ ios/longhorn_paranormal_21398Tests/longhorn_paranormal_21398Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface longhorn_paranormal_21398 : XCTestCase
+
+@end
+
+@implementation longhorn_paranormal_21398
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
